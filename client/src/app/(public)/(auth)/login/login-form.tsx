7803ba4bb455d583b52b5f35ad42fef516f47ad4
Le Nguyet Ha <lenguyetha14@gmail.com>
2024-10-04 15:04:04 +0700

    logic logout

--- client/src/app/(public)/(auth)/login/login-form.tsx
+++ client/src/app/(public)/(auth)/login/login-form.tsx
@@ -11,6 +11,7 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { useLoginMutation } from '@/queries/useAuth'
 import { toast } from '@/hooks/use-toast'
 import { handleErrorApi } from '@/lib/utils'
+import { useRouter } from 'next/navigation'
 
 export default function LoginForm() {
   const loginMutation = useLoginMutation()
@@ -21,6 +22,7 @@ export default function LoginForm() {
       password: ''
     }
   })
+  const router = useRouter()
    
    const onSubmit = async(data: LoginBodyType) => {
      if(loginMutation.isPending) return
@@ -29,6 +31,7 @@ export default function LoginForm() {
       toast({
         description: result.payload.message
       })
+      router.push('/manage/dashboard')
     }
     catch(error: any){
         handleErrorApi({
