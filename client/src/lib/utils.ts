abd940a773c5f4bff5c2f039c9da14844f34c620
Le Nguyet Ha <lenguyetha14@gmail.com>
2024-10-20 07:36:37 +0700

    Đang dùng thì refresh token hết hạn

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -44,6 +44,10 @@ export const handleErrorApi = ({
     })
   }
 }
+export const removeTokensFromLocalStorage = () => {
+  isBrowser && localStorage.removeItem('accessToken')
+  isBrowser && localStorage.removeItem('refreshToken')
+}
 export const  checkAndRefreshToken = async (param?:{
   onError?: () => void
   onSuccess?: () => void
@@ -66,8 +70,11 @@ export const  checkAndRefreshToken = async (param?:{
   // Thời điểm hết hạn của token là tính theo epoch time (s)
   // Còn khi các bạn dùng cú pháp new Date().getTime() thì nó sẽ trả về epoch time (ms)
   const now = Math.round(new Date().getTime() / 1000)
-  // trường hợp refresh token hết hạn thì không xử lý nữa
-  if (decodedRefreshToken.exp <= now) return
+  // trường hợp refresh token hết hạn thì logout
+  if (decodedRefreshToken.exp <= now) {
+    removeTokensFromLocalStorage()
+    param?.onError && param.onError()
+  }
   // Ví dụ access token của chúng ta có thời gian hết hạn là 10s
   // thì mình sẽ kiểm tra còn 1/3 thời gian (3s) thì mình sẽ cho refresh token lại
   // Thời gian còn lại sẽ tính dựa trên công thức: decodedAccessToken.exp - now
@@ -89,6 +96,7 @@ export const  checkAndRefreshToken = async (param?:{
   }
 }
 const isBrowser = typeof window !== 'undefined'
+
 
 export const getAccessTokenFromLocalStorage = () =>
   isBrowser ? localStorage.getItem('accessToken') : null
