81accc879ab3142be5e4ff984bf72b473aca29d6
Le Nguyet Ha <lenguyetha14@gmail.com>
2024-10-23 15:08:13 +0700

    khai báo API cho accounts

--- client/src/apiRequests/account.ts
+++ client/src/apiRequests/account.ts
@@ -1,15 +1,29 @@
 import http from '@/lib/http'
-import { AccountResType, ChangePasswordBodyType, UpdateMeBodyType } from '@/schemaValidations/account.schema'
+import { AccountListResType, AccountResType, ChangePasswordBodyType, CreateEmployeeAccountBodyType, UpdateEmployeeAccountBodyType, UpdateMeBodyType } from '@/schemaValidations/account.schema'
 
+const prefix = '/accounts'
 const accountApiRequest = {
-  me: () => http.get<AccountResType>('/accounts/me'), // được dùng khi đã xác thực
-  sMe: (accessToken: string) => http.get<AccountResType>('/accounts/me', {  //Sử dụng sMe khi bạn có access token và muốn gửi nó cùng với yêu cầu để xác thực.
+  me: () => http.get<AccountResType>(`${prefix}/me`), // được dùng khi đã xác thực
+  sMe: (accessToken: string) => http.get<AccountResType>(`${prefix}/me`, {  //Sử dụng sMe khi bạn có access token và muốn gửi nó cùng với yêu cầu để xác thực.
     headers:{
       Authorization: `Bearer ${accessToken}`
     } //gọi ở server component
   }),
-  updateMe: (body: UpdateMeBodyType) => http.put<AccountResType>('/accounts/me', body),
+  updateMe: (body: UpdateMeBodyType) => http.put<AccountResType>(`${prefix}/me`, body),
   changePassword: (body: ChangePasswordBodyType) => { return http.put< AccountResType>('/accounts/change-password', body)},
+  list: () => http.get<AccountListResType >(`${prefix}`),
+  addEmployee: (body: CreateEmployeeAccountBodyType) => {
+    return http.post<AccountResType>(prefix, body)
+  },
+  updateEmployee: (id: number, body: UpdateEmployeeAccountBodyType) => {
+    return http.put<AccountResType>(`${prefix}/detaild/:${id}`, body)
+  },
+  getEmployee:(id: number)=>{
+    return http.get<AccountResType>(`${prefix}/detail/:${id}`)
+  },
+  deleteEmployee:(id: number)=>{
+    return http.delete<AccountResType>(`$(prefix)/detail/:${id}`)
+  }
 
 
 }
