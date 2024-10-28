60170da8a6c54ddef5c56741d3283be6b65de7e6
Le Nguyet Ha <lenguyetha14@gmail.com>
2024-10-28 11:10:33 +0700

    sửa món ăn

--- client/src/app/manage/dishes/edit-dish.tsx
+++ client/src/app/manage/dishes/edit-dish.tsx
@@ -13,16 +13,20 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Upload } from 'lucide-react'
-import {  useMemo, useRef, useState } from 'react'
+import {  useEffect, useMemo, useRef, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { Form, FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 
-import { getVietnameseDishStatus } from '@/lib/utils'
+import { getVietnameseDishStatus, handleErrorApi } from '@/lib/utils'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
 import { UpdateDishBody, UpdateDishBodyType } from '@/schemaValidations/dish.schema'
 import { DishStatus, DishStatusValues } from '@/constants/type'
 import { Textarea } from '@/components/ui/textarea'
+import { useGetDishQuery, useUpdateDishMutation } from '@/queries/useDish'
+import { useUploadMediaMutation } from '@/queries/useMedia'
+import { describe } from 'node:test'
+import { toast } from '@/hooks/use-toast'
 
 export default function EditDish({
   id,
@@ -35,13 +39,16 @@ export default function EditDish({
 }) {
   const [file, setFile] = useState<File | null>(null)
   const imageInputRef = useRef<HTMLInputElement | null>(null)
+  const uploadMediaMutation = useUploadMediaMutation()
+  const updateDishMutation = useUpdateDishMutation()
+  const {data} = useGetDishQuery({enabled: Boolean(id), id: id as number})
   const form = useForm<UpdateDishBodyType>({
     resolver: zodResolver(UpdateDishBody),
     defaultValues: {
       name: '',
       description: '',
       price: 0,
-      image: '',
+      image: undefined,
       status: DishStatus.Unavailable
     }
   })
@@ -53,6 +60,52 @@ export default function EditDish({
     }
     return image
   }, [file, image])
+  useEffect(()=> {
+    if(data){
+      const {name, image, description, price, status} = data.payload.data
+      form.reset({
+        name,
+        image: image ?? undefined,
+        description,
+        price,
+        status
+      })
+    }
+  }, [data, form])
+  const onSubmit = async (values: UpdateDishBodyType) => {
+    if(updateDishMutation.isPending) return
+    try{
+         let body : UpdateDishBodyType & {id: number} = {
+          id: id as number,
+          ...values
+         }
+         if(file){
+          const formData = new FormData();
+          formData.append('file', file)
+          const uploadImageResult = await uploadMediaMutation.mutateAsync(formData)
+          const imageUrl = uploadImageResult.payload.data 
+          body = {
+            ...body,
+            image: imageUrl
+          }
+         }
+         const result = await updateDishMutation.mutateAsync(body)
+          toast({
+            description: result.payload.message
+          })
+          reset()
+    }
+    catch(error){
+      handleErrorApi({
+        error,
+        setError: form.setError
+      })
+    }
+  }
+  const reset = () => {
+    setId(undefined)
+    setFile(null)
+  }
   return (
     <Dialog
       open={Boolean(id)}
@@ -68,7 +121,7 @@ export default function EditDish({
           <DialogDescription>Các trường sau đây là bắ buộc: Tên, ảnh</DialogDescription>
         </DialogHeader>
         <Form {...form}>
-          <form noValidate className='grid auto-rows-max items-start gap-4 md:gap-8' id='edit-dish-form'>
+          <form noValidate className='grid auto-rows-max items-start gap-4 md:gap-8' id='edit-dish-form' onSubmit={form.handleSubmit(onSubmit)}>
             <div className='grid gap-4 py-4'>
               <FormField
                 control={form.control}
