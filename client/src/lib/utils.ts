aad59519c947f812d034e43f79b5a4346cd1f2a1
Le Nguyet Ha <lenguyetha14@gmail.com>
2024-11-16 20:59:28 +0700

    Xử lý phân quyền giữa khách và quản lý

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -8,9 +8,10 @@ import { UseFormSetError } from 'react-hook-form'
 import { twMerge } from 'tailwind-merge'
 import jwt from 'jsonwebtoken'
 import authApiRequest from '@/apiRequests/auth'
-import { DishStatus, OrderStatus, TableStatus } from '@/constants/type'
+import { DishStatus, OrderStatus, Role, TableStatus } from '@/constants/type'
 import envConfig from '@/config'
 import {TokenPayload} from '@/types/jwt.types'
+import guestApiRequest from '@/apiRequests/guest'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
@@ -82,7 +83,8 @@ export const  checkAndRefreshToken = async (param?:{
   ) {
     // Gọi API refresh token
     try {
-      const res = await authApiRequest.refreshToken()
+      const role = decodedRefreshToken.role
+    const res = role === Role.Guest ? (await guestApiRequest.refreshToken()) : await authApiRequest.refreshToken()
       setAccessTokenToLocalStorage(res.payload.data.accessToken)
       setRefreshTokenToLocalStorage(res.payload.data.refreshToken)
       param?.onSuccess && param.onSuccess()
--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,7 +1,11 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+import { decodeToken } from './lib/utils'
+import { Role } from './constants/type'
 
-const privatePaths = ['/manage']
+const managePaths = ['/manage']
+const guestPaths = ['/guests']
+const privatePaths = [...managePaths, ...guestPaths]
 const unAuthPaths = ['/login']
 
 // This function can be marked `async` if using `await` inside
@@ -17,23 +21,36 @@ export function middleware(request: NextRequest) {
     url.searchParams.set('clearTokens', 'true')
     return NextResponse.redirect(url)
   }
-  // Đăng nhập rồi thì sẽ không cho vào login nữa
-  if (unAuthPaths.some((path) => pathname.startsWith(path)) && refreshToken) {
-    return NextResponse.redirect(new URL('/', request.url))
-  } 
-  //đăng nhập rồi nhưng access token hết hạn
-  if(unAuthPaths.some(path => pathname.startsWith(path)) && !accessToken && refreshToken ){
-    const url = new URL('/refresh-token', request.url)
-    url.searchParams.set('refreshToken', refreshToken)
-    url.searchParams.set('redirect', pathname)
-    return NextResponse.redirect(url)
+  // Đã đăng nhập
+  if(refreshToken){
+    //2.1 nếu cố tình vào login thì redirect vào trang chủ
+    if(unAuthPaths.some(path => pathname.startsWith(path))){
+      return NextResponse.redirect(new URL('/', request.url))
+    }
+    //2.2 access token hết hạn
+    if(privatePaths.some(path => pathname.startsWith(path)) && !accessToken){
+      const url = new URL('/refresh-token', request.url)
+      url.searchParams.set('refreshToken', refreshToken)
+      url.searchParams.set('redirect', pathname)
+      return NextResponse.redirect(url)
+    }
+
+    // 2.3 nếu vào không đúng role thì redirect về trang chủ
+    const role = decodeToken(refreshToken).role
+    
+    if((role === Role.Guest) && managePaths.some((path) => pathname.startsWith(path)) || 
+   (role !== Role.Guest && guestPaths.some((path) => pathname.startsWith(path)))){
+       return NextResponse.redirect(new URL('/', request.url))
+    }
   }
+  // Đăng nhập rồi thì sẽ không cho vào login nữa
+  
   return NextResponse.next()
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/manage/:path*', '/login']
+  matcher: ['/manage/:path*', '/guest:path*','/login']
 }
 
 // middleware là một tính năng,
