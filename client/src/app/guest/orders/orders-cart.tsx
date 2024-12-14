3ef451c7ab5e6d60d65e1d4ac27209b578388d74
Le Nguyet Ha <lenguyetha14@gmail.com>
2024-12-14 17:41:29 +0700

    Fix bug không refetch lại khi truy cập lại component và tính toán lại hóa đơn cho khách

--- client/src/app/guest/orders/orders-cart.tsx
+++ client/src/app/guest/orders/orders-cart.tsx
@@ -2,84 +2,138 @@
 import { useGuestGetOrderListQuery } from "@/queries/useGuest";
 import { useEffect, useMemo } from "react";
 import Image from "next/image";
-import { formatCurrency, getVietNameseOrderStatus, getVietnameseOrderStatus } from "@/lib/utils";
+import {
+  formatCurrency,
+  getVietNameseOrderStatus,
+  getVietnameseOrderStatus,
+} from "@/lib/utils";
 import { Badge } from "@/components/ui/badge";
-import  socket  from "@/lib/socket";
+import socket from "@/lib/socket";
 import { UpdateOrderResType } from "@/schemaValidations/order.schema";
 import { toast } from "@/hooks/use-toast";
+import { OrderStatus } from "@/constants/type";
 
 export default function OrdersCart() {
   const { data, refetch } = useGuestGetOrderListQuery();
   const orders = useMemo(() => data?.payload.data ?? [], [data]);
-  const totalPrice = useMemo(() => {
-    return orders.reduce((result, order) => {
-      return result + order.dishSnapshot.price * order.quantity;
-    }, 0);
+  const { waitingForPaying, paid } = useMemo(() => {
+    return orders.reduce(
+      (result, order) => {
+        if (
+          order.status === OrderStatus.Delivered ||
+          order.status === OrderStatus.Processing ||
+          order.status === OrderStatus.Pending
+        ) {
+          return {
+            ...result,
+            waitingForPaying: {
+              price:
+                result.waitingForPaying.price +
+                order.dishSnapshot.price * order.quantity,
+              quantity: result.waitingForPaying.quantity + order.quantity
+            }
+          }
+        }
+        if (order.status === OrderStatus.Paid) {
+          return {
+            ...result,
+            paid: {
+              price:
+                result.paid.price + order.dishSnapshot.price * order.quantity,
+              quantity: result.paid.quantity + order.quantity
+            }
+          }
+        }
+        return result
+      },
+      {
+        waitingForPaying: {
+          price: 0,
+          quantity: 0
+        },
+        paid: {
+          price: 0,
+          quantity: 0
+        }
+      }
+    )
   }, [orders])
   useEffect(() => {
     if (socket.connected) {
-      onConnect()
+      onConnect();
     }
     function onConnect() {
-      console.log(socket.id)
+      console.log(socket.id);
     }
     function onDisconnect() {
-      console.log('disconnect')
+      console.log("disconnect");
     }
-    function onUpdateOrder(data: UpdateOrderResType['data']) {
-        console.log(data)
-        const {dishSnapshot: {name}, quantity} = data
-        toast({
-          description: `Món ${name} (SL: ${quantity}) vừa được cập nhật sang trạng thái "${getVietNameseOrderStatus(data.status)}"`
-        })
-      refetch()
+    function onUpdateOrder(data: UpdateOrderResType["data"]) {
+      console.log(data);
+      const {
+        dishSnapshot: { name },
+        quantity,
+      } = data;
+      toast({
+        description: `Món ${name} (SL: ${quantity}) vừa được cập nhật sang trạng thái "${getVietNameseOrderStatus(
+          data.status
+        )}"`,
+      });
+      refetch();
     }
-    socket.on('update-order', onUpdateOrder)
-    socket.on('connect', onConnect)
-    socket.on('disconnect', onDisconnect)
+    socket.on("update-order", onUpdateOrder);
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
     return () => {
-      socket.off('connect', onConnect)
-      socket.off('disconnect', onDisconnect)
-      socket.off('update-order', onUpdateOrder)
-    }
-  }, [refetch])
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+      socket.off("update-order", onUpdateOrder);
+    };
+  }, [refetch]);
 
   return (
     <>
       {orders.map((order, index) => (
-        <div key={order.id} className='flex gap-4 mb-4'>
-          <div className='text-sm font-semibold'>{index + 1}</div>
-          <div className='flex-shrink-0 relative'>
+        <div key={order.id} className="flex gap-4 mb-4">
+          <div className="text-sm font-semibold">{index + 1}</div>
+          <div className="flex-shrink-0 relative">
             <Image
               src={order.dishSnapshot.image}
               alt={order.dishSnapshot.name}
               height={100}
               width={100}
               quality={100}
-              className='object-cover w-[80px] h-[80px] rounded-md'
+              className="object-cover w-[80px] h-[80px] rounded-md"
             />
           </div>
-          <div className='space-y-1'>
-            <h3 className='text-sm'>{order.dishSnapshot.name}</h3>
-            <div className='text-xs font-semibold'>
-              {formatCurrency(order.dishSnapshot.price)} x{' '}
-              <Badge className='px-1'>{order.quantity}</Badge>
+          <div className="space-y-1">
+            <h3 className="text-sm">{order.dishSnapshot.name}</h3>
+            <div className="text-xs font-semibold">
+              {formatCurrency(order.dishSnapshot.price)} x{" "}
+              <Badge className="px-1">{order.quantity}</Badge>
             </div>
           </div>
-          <div className='flex-shrink-0 ml-auto flex justify-center items-center'>
-            <Badge variant={'outline'}>
+          <div className="flex-shrink-0 ml-auto flex justify-center items-center">
+            <Badge variant={"outline"}>
               {getVietnameseOrderStatus(order.status)}
             </Badge>
           </div>
         </div>
       ))}
-      <div className='sticky bottom-0 '>
-        <div className='w-full flex space-x-4 text-xl font-semibold'>
-          <span>Tổng cộng · {orders.length} món</span>
-          <span>{formatCurrency(totalPrice)}</span>
+        {paid.quantity !== 0 && (
+        <div className='sticky bottom-0 '>
+          <div className='w-full flex space-x-4 text-xl font-semibold'>
+            <span>Đã thanh toán · {paid.quantity} món</span>
+            <span>{formatCurrency(paid.price)}</span>
+          </div>
+        </div>
+      )}
+      <div className="sticky bottom-0 ">
+        <div className="w-full flex space-x-4 text-xl font-semibold">
+        <span>Chưa thanh toán · {waitingForPaying.quantity} món</span>
+        <span>{formatCurrency(waitingForPaying.price)}</span>
         </div>
       </div>
     </>
-  )
-
+  );
 }
